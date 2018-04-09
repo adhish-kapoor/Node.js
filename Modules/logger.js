5e7ce6bc2b6135f3ebf747dd889f19ef8ee06fef
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2018-04-09 17:15:36 +0530

    Update logger.js

--- Modules/logger.js
+++ Modules/logger.js
@@ -1,10 +1,24 @@
+// var url="http://mylogger.io"
+
+// function log(message){
+// //send an http request
+// console.log(message)
+
+// }
+// module.exports.log=log //exports used to make log public,logger is an object
+
+// module.exports=log    //logger is a function and not an object
+
+//making a class to raise event
+const EventEmitter=require("events")
 var url="http://mylogger.io"
 
-function log(message){
-//send an http request
-console.log(message)
+class Logger extends EventEmitter{
+     log(message) { //method in class
+        console.log(message)
 
+        //Raise an event
+        this.emit('MessageLogged',{id:1,name:"Adhish"})
+    }
 }
-module.exports.log=log //exports used to make log public,logger is an object
-
-module.exports=log    //logger is a function and not an object
+module.exports=Logger;
