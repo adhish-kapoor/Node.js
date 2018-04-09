f7cee478089078d1a2050186a1257603afad0646
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2018-04-09 17:18:02 +0530

    Update app.js

--- Modules/app.js
+++ Modules/app.js
@@ -1,7 +1,18 @@
 //console.log(module)
-var logger=require('./logger.js')//'require' returns object exported from target module and 
+// var logger=require('./logger.js')//'require' returns object exported from target module and 
 //is used to load a module
 
 //console.log(logger)
 
-logger('hello')
+// logger('hello')
+
+const EventEmitter=require("events")
+
+const Logger=require('./logger')
+const logger=new Logger();
+//Register a listener
+logger.on("MessageLogged",(args)=>{
+    console.log("Listener called",args) //output on running node app.js 
+                                        //Message 
+})                                      // Listener called { id: 1, name: 'Adhish' }
+logger.log('Message')
