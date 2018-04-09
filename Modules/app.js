f505b0c22e5999c634f5e7809d1038b8a9538ccf
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2018-04-09 15:10:31 +0530

    Create app.js

---
+++ Modules/app.js
@@ -0,0 +1,7 @@
+//console.log(module)
+var logger=require('./logger.js')//'require' returns object exported from target module and 
+//is used to load a module
+
+//console.log(logger)
+
+logger('hello')
